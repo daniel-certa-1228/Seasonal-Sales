0ed4a04362a4dccbc416c5a869c324cbaacae1a1
daniel-certa-1228 <mfdancerta@gmail.com>
2017-07-31 22:07:59 -0500

    Mostly working- need to figure out price discount

--- js/seasonal-sales.js
+++ js/seasonal-sales.js
@@ -1,18 +1,48 @@
 console.log( "seasonal-sales.js" );
 
-function showProductInfo(productObject) {
-	console.log( "productObject", productObject );
-	let products = productObject.products;
-	console.log( "products", products );
+let outputDiv = document.getElementById("product-div");
 
-	for (let i = 0; i < products.length; i++) {
-		 // if products.
-	};
+{
+	StoreInfo.showProductInfo = (productObject) => {
+		// console.log( "productObject", productObject );
+		let products = productObject.products;
+		// console.log( "products", products );
+		let apparelCard = `<div class="product-card" id="apparel"><h2>Apparel</h2>`
+		let furnitureCard = `<div class="product-card" id="furniture"><h2>Furniture</h2>`
+		let hardwareCard = `<div class="product-card" id="hardware"><h2>Hardware</h2>`
+		for (let i = 0; i < products.length; i++) {
+			 if (products[i].category_id === 1) {
+			 	
+			 	apparelCard += `<section class="item" id="item--${i}">
+			 					<h4>${products[i].name}</h4>
+			 					<h5>Price - $${products[i].price}</h5>
+			 					</section>`
+			 	
+			 }  else if  (products[i].category_id === 2) {
+
+			 	furnitureCard += `<section class="item" id="item--${i}">
+			 					<h4>${products[i].name}</h4>
+			 					<h5>Price - $${products[i].price}</h5>
+			 					</section>`
+
 	
+			 }  else if (products[i].category_id === 3) {
+			 	
+			 	hardwareCard += `<section class="item" id="item--${i}">
+			 					<h4>${products[i].name}</h4>
+			 					<h5>Price - $${products[i].price}</h5>
+			 					</section>`
+			 }
+		};
 
+				apparelCard += `<h3 class="hidden red" id="apparel-sale">SALE! 15% OFF!</h3></div>`
+				outputDiv.innerHTML += apparelCard
+				furnitureCard += `<h3 class="hidden red" id="furniture-sale">SALE! 15% OFF!</h3></div>`
+				outputDiv.innerHTML += furnitureCard
+				hardwareCard += `<h3 class="hidden red" id="hardware-sale">SALE! 15% OFF!</h3></div>`
+				outputDiv.innerHTML += hardwareCard
+	}
 }
-
-
 
 
 StoreInfo.loadProductInfo();
